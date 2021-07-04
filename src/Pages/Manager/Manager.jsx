3b8ac9b97fa579c8c47786f99a5ad8ec3d18243b
Ksenia Gountsova <ksusha.net@gmail.com>
2021-07-04 23:51:41 +0300

    manager

--- src/Pages/Manager/Manager.jsx
+++ src/Pages/Manager/Manager.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react'
+import Card from '../../Components/Card/Card'
+import Form from '../../Components/Form/Form'
+import {editWord, removeWord} from '../../Utils/functions'
 
 
 const Manager = ({words})=> {
+    let wordsCollection = words.map( word => {
+        return( <section className="word-wrapper" >
+        <Card word='Ksenia' lang='eng' />
+        <section className="buttons-container">
+        <button onClick = {editWord}>Edit Word</button>
+        <button onClick = {removeWord}>Delete Word</button>
+        </section>
+        </section>
+    );
+});
 
-    return (
-    <div>
-        <h1>Vocabulary</h1>
-         
+
+    return (<div>
+         <Form/>
+        <div className="vocabulary">
+       {wordsCollection}
     </div>
-
+    </div>
     )
 }
 
